247fc9078818958d57e50faeea956f3ec6439113
JuanAguayo <aguayo-96@hotmail.com>
2015-05-08 15:39:16 -0500

    Hola Mundo
    Aplicacion de android (hola mundo )

---
+++ Holamundo/app/src/androidTest/java/com/example/cnu40196yt/holamundo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.cnu40196yt.holamundo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
